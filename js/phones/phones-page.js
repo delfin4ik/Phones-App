9efd3817de4bbca3a97bf29640cc027898270e76
Aleksandr Pryshchepa <alexandrsan@ukr.net>
2019-01-31 22:46:30 +0200

    PhoneViewer show was implemented

--- js/phones/phones-page.js
+++ js/phones/phones-page.js
@@ -11,7 +11,15 @@ export default class PhonesPage {
     this._catalog = new PhoneCatalog({
       element: document.querySelector('[data-component="phone-catalog"]'),
       phones: PhoneService.getAll(),
+
+      onPhoneSelected: (phoneId) => {
+        const phoneDetails = PhoneService.getById(phoneId);
+
+        this._catalog.hide();
+        this._viewer.show(phoneDetails);
+      },
     });
+    
     this._viewer = new PhoneViever({
       element: document.querySelector('[data-component="phone-viewer"]')
     });
